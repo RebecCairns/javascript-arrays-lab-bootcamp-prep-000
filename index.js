2702bb711b8f9738a2fa4d4e1517b762c43374b2
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-15 21:55:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(name);
+  return kittens;
+}
+function appendKitten(name){
+  var newArray = kittens;
+  newArray = newArray.concat(name);
+  return newArray;
+}
+function prependKitten(name){
+  var newArray[];
+  newArray[0] = name;
+  newArray = newArray.concat(name);
+  return newArray;
+}
+//removeLastKitten()r
+//removeFirstKitten()
 
 // Add your functions and code here
